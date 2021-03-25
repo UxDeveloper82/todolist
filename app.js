f96c2d3606b764e9350ac583839ba67809432b08
UxDeveloper82 <electronicsmf@aol.com>
2021-03-25 18:30:08 -0400

    adding checklist

--- app.js
+++ app.js
@@ -42,9 +42,18 @@ function deleteCheck(e) {
    //Delete TODO
    if(item.classList[0] === 'trash-btn') {
        const todo = item.parentElement;
-       todo.remove();
+       //Animation
+       todo.classList.add('fall');
+       todo.addEventListener('transitionend', function() {
+           todo.remove();
+       })      
    }
 
+   //Chech Mark 
+   if(item.classList[0] === 'complete-btn') {
+     const todo = item.parentElement;
+     todo.classList.toggle('completed');
+   }
 }
 
 
